869f59c2af88a944625d76d95659bab0d1d552d3
Max Rhino <153639807+MaxURhino@users.noreply.github.com>
2026-03-24 06:48:24 +0100

    Add FlowingFluid constants for ACID

---
+++ reference/latest/src/main/java/com/example/docs/fluid/ModFluids.java
@@ -0,0 +1,8 @@
+package com.example.docs.fluid;
+
+import net.minecraft.world.level.material.FlowingFluid;
+
+public class ModFluids {
+    public static final FlowingFluid ACID_FLOWING;
+    public static final FlowingFluid ACID_STILL
+}
